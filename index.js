489fa6ee50bafe5f29e1d499d9de5ddf556eb0da
yadonoya <mscottschreiber@gmail.com>
2022-04-28 11:01:11 +0900

    feat: update index.js to use barsController.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const test = require('./controller/barsController');
+const bars = require('./controller/barsController');
 const app = express();
 
 app.use(express.json());
+app.use(bars);
 
 app.listen('3000', () => {
     console.log('Server is listening at port 3000');
